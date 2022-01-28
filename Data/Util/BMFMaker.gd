1b39e5d1e6197bf950f0259a0bfb71c147af6d5f
Leandro Sotelo <66535294+loopies2600@users.noreply.github.com>
2022-01-28 19:21:15 -0300

    BMFMaker 3

--- Data/Util/BMFMaker.gd
+++ Data/Util/BMFMaker.gd
@@ -1,10 +1,11 @@
 extends Label
 
 export (Vector2) var charSize = Vector2(8, 8)
-export (int) var charSpacing = 8
+export (Vector2) var charSpacing = Vector2(8, 8)
 export (Texture) var texture
-export (PoolStringArray) var columns
 export (String) var path = "res://Sprites/Font/Main.tres"
+export (Vector2) var tallCharOffset = Vector2()
+export (PoolStringArray) var columns
 
 onready var bm := BitmapFont.new()
 
@@ -21,11 +22,15 @@ func generate():
 		for c in range(columns[i].length()):
 			var charPos := Vector2(charSize.x * row, charSize.y * column)
 			var unicode = ord(columns[i][c])
-			bm.add_char(unicode, 0, Rect2(charPos, Vector2(charSize.x, charSize.y)), Vector2(), charSpacing)
+			
+			var offset : Vector2 = tallCharOffset if ord(columns[i][c]) in [103, 112, 113, 121] else Vector2()
+			
+			bm.add_char(unicode, 0, Rect2(charPos, Vector2(charSize.x, charSize.y)), offset, charSpacing.x)
 			
 			row += 1
 			
 		row = 0
 		column += 1
 		
+	bm.height = charSpacing.y
 	ResourceSaver.save(path, bm)
