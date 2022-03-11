e4285be76e055a75e5675c395284137ab71309f8
Leandro Sotelo <66535294+loopies2600@users.noreply.github.com>
2022-03-11 19:41:59 -0300

    Elpibe

--- Data/Object/SpawnPoint.gd
+++ Data/Object/SpawnPoint.gd
@@ -1,13 +1,4 @@
 extends Sprite
 
-var y := 0.0
-var _time := 0.0
-
 func _process(delta):
 	visible = Main.editing
-	
-	if !Main.editing: return
-	
-	_time += delta
-	
-	offset.y += sin(_time * 2) * 0.25 
